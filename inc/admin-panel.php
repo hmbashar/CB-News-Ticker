327b6ffad00e6fb78867c7e23d852073c4f11c44
hmbashar <hmbashar@gmail.com>
2021-02-08 13:28:32 +0600

    add more features

--- inc/admin-panel.php
+++ inc/admin-panel.php
@@ -9,11 +9,17 @@ function cb_news_ticker_fields_register() {
 	// Add Section
 	add_settings_section( 'cb_news_ticker_section', __('Setting for CB News Ticker', 'CBNT'), 'cb_news_ticker_section_desc', 'cb_news_ticker.php' );
 	
-	// Add Field
-	add_settings_field( 'cb-news-ticker-news-text-color', __('Text Color', 'CBNT'), 'cb_news_ticker_news_text_color', 'cb_news_ticker.php', 'cb_news_ticker_section' );
-		
-	// Add Field
-	add_settings_field( 'cb-news-ticker-news-bg-color', __('Background Color', 'CBNT'), 'cb_news_ticker_news_bg_color', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+
+	// Add Field for choose design
+	add_settings_field( 'cb-news-ticker-design', __('Choose your Design', 'CBNT'), 'cb_news_ticker_design', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+	
+	// Add Field for post or custom switch button
+	add_settings_field( 'cb-news-ticker-ts-button', __('Type', 'CBNT'), 'cb_news_ticker_ts_button', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+	
+
+	// Add Field for custom text
+	add_settings_field( 'cb-news-ticker-custom-text', __('Custom', 'CBNT'), 'cb_news_ticker_custom_text', 'cb_news_ticker.php', 'cb_news_ticker_section' );	
+	
 	
 	// Add Field for post id
 	add_settings_field( 'cb-news-ticker-post-id', __('Post ID', 'CBNT'), 'cb_news_ticker_post_id', 'cb_news_ticker.php', 'cb_news_ticker_section' );
@@ -21,35 +27,43 @@ function cb_news_ticker_fields_register() {
 	// Add Field for post count
 	add_settings_field( 'cb-news-ticker-post-count', __('Count', 'CBNT'), 'cb_news_ticker_post_count', 'cb_news_ticker.php', 'cb_news_ticker_section' );
 	
+
 	// Add Field for Breaking news text
 	add_settings_field( 'cb-news-ticker-bn-text', __('Breaking News', 'CBNT'), 'cb_news_ticker_breaking_news', 'cb_news_ticker.php', 'cb_news_ticker_section' );
 	
+
+	// Add Field
+	add_settings_field( 'cb-news-ticker-news-text-color', __('Text Color', 'CBNT'), 'cb_news_ticker_news_text_color', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+	// Add Field
+	add_settings_field( 'cb-news-ticker-bn-text-color', __('BN Text Color', 'CBNT'), 'cb_news_ticker_bn_text_color', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+		
+	// Add Field
+	add_settings_field( 'cb-news-ticker-news-bg-color', __('Background Color', 'CBNT'), 'cb_news_ticker_news_bg_color', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+
+	// Add Field for breaking new background color
+	add_settings_field( 'cb-news-ticker-bn-bg-color', __('BN Background Color', 'CBNT'), 'cb_news_ticker_bn_bg_color', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+
+
+
 	// Add Field for close button
 	add_settings_field( 'cb-news-ticker-close-button', __('Switch Close Button', 'CBNT'), 'cb_news_ticker_close_button', 'cb_news_ticker.php', 'cb_news_ticker_section' );
-	
-	// Add Field for post or custom switch button
-	add_settings_field( 'cb-news-ticker-ts-button', __('Post/Custom Switch', 'CBNT'), 'cb_news_ticker_ts_button', 'cb_news_ticker.php', 'cb_news_ticker_section' );
-	
-	// Add Field for custom text
-	add_settings_field( 'cb-news-ticker-custom-text', __('Custom', 'CBNT'), 'cb_news_ticker_custom_text', 'cb_news_ticker.php', 'cb_news_ticker_section' );	
 
 	// Add Field for choose design
-	add_settings_field( 'cb-news-ticker-design', __('Choose your Design', 'CBNT'), 'cb_news_ticker_design', 'cb_news_ticker.php', 'cb_news_ticker_section' );
-	
-	// Add Field for choose design
-	add_settings_field( 'cb-news-ticker-post-cat', __('Choose your Cat', 'CBNT'), 'cb_news_ticker_cat_slug', 'cb_news_ticker.php', 'cb_news_ticker_section' );
+	//add_settings_field( 'cb-news-ticker-post-cat', __('Choose your Cat', 'CBNT'), 'cb_news_ticker_cat_slug', 'cb_news_ticker.php', 'cb_news_ticker_section' );
 
 	// Register field
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-news-text-color', array('sanitize_callback' => 'esc_attr') );
+	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-bn-text-color', array('sanitize_callback' => 'esc_attr') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-news-bg-color', array('sanitize_callback' => 'esc_attr') );
-	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-post-id', array('sanitize_callback' => 'esc_attr') );
+	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-bn-bg-color', array('sanitize_callback' => 'esc_attr') );
+	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-post-id', array('sanitize_callback' => 'esc_html') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-post-count', array('sanitize_callback' => 'esc_attr') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-bn-text', array('sanitize_callback' => 'esc_attr') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-custom-text', array('sanitize_callback' => 'esc_html') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-close-button', array('sanitize_callback' => 'esc_attr') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-ts-button', array('sanitize_callback' => 'esc_attr') );
 	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-design', array('sanitize_callback' => 'esc_attr') );
-	register_setting( 'cb_news_ticker_section', 'cb-news-ticker-post-cat', array('sanitize_callback' => 'esc_attr') );
+	//register_setting( 'cb_news_ticker_section', 'cb-news-ticker-post-cat', array('sanitize_callback' => 'esc_attr') );
 }
 add_action('admin_init', 'cb_news_ticker_fields_register');
 
@@ -65,6 +79,17 @@ function cb_news_ticker_news_text_color() {
 	$cbnt_text_id = 'cb-news-ticker-news-text-color';	
 
 	printf("<input type='text' id='%s' name='%s' class='regular-text'  value='%s' placeholder='Input your color code/name'>", $cbnt_text_id, $cbnt_text_id, $cbnt_text_color_val);
+
+	printf('%s Please type your color name or color code with a # sign, it will apply to content text %s', '<p class="description">', '</p>');
+}
+// BN Text Color Field
+function cb_news_ticker_bn_text_color() {
+	$bn_text_color_val = get_option('cb-news-ticker-bn-text-color');
+	$bn_text_id = 'cb-news-ticker-bn-text-color';	
+
+	printf("<input type='text' id='%s' name='%s' class='regular-text'  value='%s' placeholder='Input your color code/name'>", $bn_text_id, $bn_text_id, $bn_text_color_val);
+
+	printf('%s Please type your color name or color code with a # sign, it will apply to Heading/Alert text %s', '<p class="description">', '</p>');
 }
 
 // News Background Color Field
@@ -73,6 +98,19 @@ function cb_news_ticker_news_bg_color() {
 	$cbnt_bg_id = 'cb-news-ticker-news-bg-color';	
 
 	printf("<input type='text' id='%s' name='%s' class='regular-text'  value='%s' placeholder='Input your color code/name'>", $cbnt_bg_id, $cbnt_bg_id, $cbnt_bg_color_val);
+
+	printf('%s Please type your color name or color code with a # sign, it will apply to full width news ticker section %s', '<p class="description">', '</p>');
+}
+
+// News BN Background Color Field
+function cb_news_ticker_bn_bg_color() {
+	$bn_bg_color_val = get_option('cb-news-ticker-bn-bg-color');
+	$bn_bg_id = 'cb-news-ticker-bn-bg-color';	
+
+	printf("<input type='text' id='%s' name='%s' class='regular-text'  value='%s' placeholder='Input your color code/name'>", $bn_bg_id, $bn_bg_id, $bn_bg_color_val);
+
+
+	printf('%s Please type your color name or color code with a # sign, it will apply to Heading/Alert text background %s', '<p class="description">', '</p>');
 }
 
 
@@ -82,14 +120,15 @@ function cb_news_ticker_post_id() {
 	$cbnt_post_id = 'cb-news-ticker-post-id';	
 
 	printf("<input type='text' id='%s' name='%s' class='regular-text'  value='%s' placeholder='News ID'>", $cbnt_post_id, $cbnt_post_id, $cb_news_id_val);
-	printf('%s Please input your post id here, you can find your post id from your all post option, check the screenshot https://prnt.sc/rwbf8l %s', '<p class="description">', '</p>');
+
+	printf('%s Please input your post id here, you can find your post id from your all post option, check the screenshot https://prnt.sc/rwbf8l <b>Important: only work if you select "Post" from the type field</b> %s', '<p class="description">', '</p>');
 	
 }
 
 
 
 
-// News Ticker Post ID count
+// News Ticker Post count
 function cb_news_ticker_post_count() {
 	$cb_news_count = get_option('cb-news-ticker-post-count');
 	$cbnt_post_count = 'cb-news-ticker-post-count';	
@@ -108,6 +147,7 @@ function cb_news_ticker_breaking_news() {
 	$breaking_news = $cb_news_breaking ? $cb_news_breaking : __('Breaking News', 'CBNT');
 
 	printf("<input type='text' id='%s' name='%s' class='regular-text'  value='%s' placeholder='Breaking News Text'>", $cbnt_post_breaking, $cbnt_post_breaking, $breaking_news);
+
 	printf('%s You can change the heading for breaking news static text, default Breaking News %s', '<p class="description">', '</p>');
 	
 }
@@ -119,7 +159,8 @@ function cb_news_ticker_custom_text() {
 	$custom_text = $cb_custom_text ? $cb_custom_text : __('Custom Text', 'CBNT');
 
 	printf("<textarea id='%s' name='%s' class='regular-text' rows='10' class='large-text code' placeholder='Breaking News Text'> %s </textarea>", $cbnt_custom_text, $cbnt_custom_text, $custom_text);
-	printf('%s You can change the heading for breaking news static text, default custom text %s', '<p class="description">', '</p>');
+
+	printf('%s Type your content here if you want to show the custom text to your ticker section, <b>Important: only work if you select "custom" from the type field</b> %s', '<p class="description">', '</p>');
 	
 }
 
@@ -160,7 +201,7 @@ function cb_news_ticker_ts_button() {
 	
 	$button_off_switch = sprintf("<input type='radio' id='%s' name='%s' class='regular-text'  value='%s' %s>",'cb-news-ts-button-off', $ts_button_id, 2, $button_off);	
 	printf('%s %s Custom %s', '<label for="cb-news-ts-button-off">',$button_off_switch, '</label>');
-	printf('%s You can show/hide your breaking news closing button. %s', '<p class="description">', '</p>');
+	printf('%s You can choose what type of data would you like to show. %s', '<p class="description">', '</p>');
 	
 }
 
--- inc/shortcode.php
+++ inc/shortcode.php
@@ -8,36 +8,48 @@ function cb_news_ticker($attrs, $content = NULL) {
 	ob_start();
 	extract(shortcode_atts(array(
 		'post_type'		=> 'post',
-		'count'			=> 1,
+		'count'			=> '',
 		'id'			=> '',
 		'bg_color'		=> '',
 		'color'			=> '',
 		'bn_text'		=> __('Breaking News', 'CBNT'),
+		'bn_bg'			=> '',
+		'bn_color'		=> '',
 		'button'		=> '',
 		'design'		=> '',
 		'cat_slug'		=> '',
+		'type'			=> '',
+		'custom_text'	=> '',
 	), $attrs));
 
 
 		//getting value from dashboard
 		$cbnt_text_color = get_option('cb-news-ticker-news-text-color'); // text color
+		$bn_txt_color = get_option('cb-news-ticker-bn-text-color'); // text color
 		$cbnt_bg_color_val = get_option('cb-news-ticker-news-bg-color'); // background color
+		$bn_bg_color_val = get_option('cb-news-ticker-bn-bg-color'); // breaking background color
 		$cb_news_id_val = get_option('cb-news-ticker-post-id'); // post id
 		$cb_news_count = get_option('cb-news-ticker-post-count');// post count
 		$cb_news_breaking = get_option('cb-news-ticker-bn-text'); // Breaking News Static Text
 		$close_button = get_option('cb-news-ticker-close-button'); // on/off cross button
 		$ticker_design = get_option('cb-news-ticker-design'); // selected design
 		$category_name = get_option('cb-news-ticker-post-cat'); // category name
+		$data_type = get_option('cb-news-ticker-ts-button'); // type like post or custom data
+		$custom_txt = get_option('cb-news-ticker-custom-text'); // custom text
 
 		// check user shortcode value for else working default value
 		$text_color = $color ? $color : $cbnt_text_color; // text color
+		$bn_color = $bn_color ? $bn_color : $bn_txt_color; // text color
 		$bg_color = $bg_color ? $bg_color : $cbnt_bg_color_val; // background color
+		$bn_bg_color = $bn_bg ? $bn_bg : $bn_bg_color_val; // background color
 		$id = $id ? $id : $cb_news_id_val; // post id
 		$count = $count ? $count : $cb_news_count; // post count
 		$bn_text = $bn_text ? $bn_text : $cb_news_breaking; // breaking news text
 		$button = $button ? $button : $close_button; // cross button on/off
 		$design = $design ? $design : $ticker_design; // change design
 		$cat_slug = $cat_slug ? $cat_slug : $category_name;
+		$type = $type ? $type : $data_type; // data type switch like post or custom text
+		$custom_text = $custom_text ? $custom_text : $custom_txt; // custom text
 
 
 		$button = $button ? $button : 1; // set default value for showing cross button
@@ -48,8 +60,8 @@ function cb_news_ticker($attrs, $content = NULL) {
 	    <div class="cb-news-ticker-breaking-news-ticker clearfix">
             <div class="cb-news-ticker-left">
                 <div class="cb-news-ticker-breaking-news">
-                    <div class="cb-news-ticker-breaking-news-heading">
-                        <strong <?php if(!empty($text_color)) : ?> style="color:<?php echo esc_attr($text_color); ?>" <?php endif; ?>><?php echo esc_html($bn_text); ?></strong>
+                    <div class="cb-news-ticker-breaking-news-heading" <?php if(!empty($bn_bg_color)) : ?>  style="background-color:<?php echo esc_attr($bn_bg_color); endif;?>">
+                        <strong <?php if(!empty($bn_color)) : ?> style="color:<?php echo esc_attr($bn_color); ?>" <?php endif; ?>><?php echo esc_html($bn_text); ?></strong>
                     </div>
                     <div class="cb-news-ticker-breaking-news-content">
                     	<div class="cb-news-ticker-marquee"> 
@@ -58,20 +70,33 @@ function cb_news_ticker($attrs, $content = NULL) {
 							<marquee class="cb-news-ticker-marquee" onmouseover="this.stop();" onmouseout="this.start();">
 							<?php endif; ?>
 								<ul>
-			                        <?php                                 
-			                            $cb_news_ticker = new WP_Query(array(
-			                                'post_type'     	=> $post_type,
-			                                'posts_per_page' 	=> $count, 
-			                                'p'       			=> $id,
-			                                'category_name'		=> $cat_slug,
-			                            ));
-			                            if($cb_news_ticker->have_posts()) : while($cb_news_ticker->have_posts()) : $cb_news_ticker->the_post();
-			                        ?>
-			                                              
+									<?php 
+										if($type == 1) : // post data type condition
 
-			                        	<li><a href="<?php the_permalink(); ?>" <?php if(!empty($text_color)) : ?> style="color:<?php echo esc_attr($text_color); ?>" <?php endif; ?>><?php the_title(); ?></a></li>
-			                        
-			                        <?php endwhile; endif; ?>
+				                            $cb_news_ticker = new WP_Query(array(
+				                                'post_type'     	=> $post_type,
+				                                'posts_per_page' 	=> $count, 
+				                                'post__in'       			=> array($id),
+				                               // 'category_name'		=> $cat_slug,
+				                            ));
+				                            if($cb_news_ticker->have_posts()) : while($cb_news_ticker->have_posts()) : $cb_news_ticker->the_post();
+				                        ?>
+				                                              
+
+				                        	<li><a href="<?php the_permalink(); ?>" <?php if(!empty($text_color)) : ?> style="color:<?php echo esc_attr($text_color); ?> !important;" <?php endif; ?>><?php the_title(); ?></a></li>
+				                        
+				                        <?php endwhile; endif; 
+			                    	endif; 
+
+			                    	// custom text condition
+			                    	if($type == 2) :
+
+			                    	?>
+
+			                    		<li><a <?php if(!empty($text_color)) : ?> style="color:<?php echo esc_attr($text_color); ?>" <?php endif; ?>><?php echo esc_html($custom_text); ?></a></li>
+
+			                    	<?php endif; ?>
+
 								</ul>
 							<?php if($design == 'Marquee') : ?> 
 							</marquee>
@@ -90,7 +115,7 @@ function cb_news_ticker($attrs, $content = NULL) {
 	            </div>
         	<?php endif; ?>
 	    </div>
-	</section><!--/ Suga Breaking News Ticker -->
+	</section><!--/ Breaking News Ticker -->
 
 
 	<?php 
